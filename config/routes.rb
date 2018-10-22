ac024de88269d41b4df2e10b76bc08f503605ddb
suginoakihiro <uhor6696@yahoo.co.jp>
2018-10-22 17:50:59 +0900

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/contact'
   get 'static_pages/about'
   root 'static_pages#home'
+  get 'user/new'
 end
