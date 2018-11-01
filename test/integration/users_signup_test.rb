371b62b232abc6f77e0e8f17d50cc253af11be5e
suginoakihiro <uhor6696@yahoo.co.jp>
2018-11-01 21:22:41 +0900

    Add password reset ...error

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -38,9 +38,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_not is_logged_in?
     get edit_account_activation_path(user.activation_token, email: user.email)
 
-    # assert user.reload.activated?
-    # follow_redirect!
-    # assert_template 'users/show'
-    # assert is_logged_in?
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
